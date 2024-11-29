bea1fc3a5f2ef06830c751498f7cb046179f2894
Will Bushell <williambushell7@gmail.com>
2024-11-29 13:38:25 -0600

    .

--- First_Test/sketch.js
+++ First_Test/sketch.js
@@ -1,17 +1,16 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight); // Set the canvas size to 800x600
-  background(200); // Set the background color to light gray
+  createCanvas(windowWidth, windowHeight); 
+  background(255); 
 }
 
 function draw() {
-  if (mouseIsPressed) { // Check if the mouse button is pressed
-    // Generate a random color
+  if (mouseIsPressed) { 
     let r = random(255);
     let g = random(255);
     let b = random(255);
 
-    fill(r, g, b); // Set the fill color
-    noStroke(); // Remove the outline of the circle
-    ellipse(mouseX, mouseY, 50, 50); // Draw a circle at the mouse position
+    fill(r, g, b); 
+    noStroke(); 
+    ellipse(mouseX, mouseY, 50, 50); 
   }
 }
